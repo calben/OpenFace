62abc97b3830b621920301b7645b21c3be1c7e21
Calem Bendell <calem.j.bendell@gmail.com>
2018-01-19 15:15:27 -0500

    support custom width, height, framerate for video capture

--- exe/FaceLandmarkVid/FaceLandmarkVid.cpp
+++ exe/FaceLandmarkVid/FaceLandmarkVid.cpp
@@ -65,10 +65,10 @@ std::cout << "Warning: " << stream << std::endl
 #define ERROR_STREAM( stream ) \
 std::cout << "Error: " << stream << std::endl
 
-static void printErrorAndAbort( const std::string & error )
+static void printErrorAndAbort(const std::string & error)
 {
-    std::cout << error << std::endl;
-    abort();
+	std::cout << error << std::endl;
+	abort();
 }
 
 #define FATAL_STREAM( stream ) \
@@ -81,7 +81,7 @@ vector<string> get_arguments(int argc, char **argv)
 
 	vector<string> arguments;
 
-	for(int i = 0; i < argc; ++i)
+	for (int i = 0; i < argc; ++i)
 	{
 		arguments.push_back(string(argv[i]));
 	}
@@ -165,28 +165,28 @@ void visualise_tracking(cv::Mat& captured_image, cv::Mat_<float>& depth_image, c
 }
 
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 
 	vector<string> arguments = get_arguments(argc, argv);
 
 	// Some initial parameters that can be overriden from command line	
 	vector<string> files, depth_directories, output_video_files, out_dummy;
-	
+
 	// By default try webcam 0
 	int device = 0;
 
 	LandmarkDetector::FaceModelParameters det_parameters(arguments);
 
 	// Get the input output file parameters
-	
+
 	// Indicates that rotation should be with respect to world or camera coordinates
 	bool u;
 	string output_codec;
 	LandmarkDetector::get_video_input_output_params(files, depth_directories, out_dummy, output_video_files, u, output_codec, arguments);
-	
+
 	// The modules that are being used for tracking
-	LandmarkDetector::CLNF clnf_model(det_parameters.model_location);	
+	LandmarkDetector::CLNF clnf_model(det_parameters.model_location);
 
 	// Grab camera parameters, if they are not defined (approximate values will be used)
 	float fx = 0, fy = 0, cx = 0, cy = 0;
@@ -206,21 +206,21 @@ int main (int argc, char **argv)
 	}
 
 	// If multiple video files are tracked, use this to indicate if we are done
-	bool done = false;	
+	bool done = false;
 	int f_n = -1;
-	
+
 	det_parameters.track_gaze = true;
 
 
 	//Action Units Extraction
 
 	double time_stamp = 0;
-	
+
 	// Search paths for AU models
 	boost::filesystem::path config_path = boost::filesystem::path(CONFIG_DIR);
 	boost::filesystem::path parent_path = boost::filesystem::path(arguments[0]).parent_path();
 	string au_loc;
-	
+
 	double sim_scale = -1;
 	int sim_size = 112;
 	bool dynamic = true; // Indicates if a dynamic AU model should be used (dynamic is useful if the video is long enough to include neutral expressions)
@@ -263,13 +263,13 @@ int main (int argc, char **argv)
 	{
 		au_loc = au_loc_path.string();
 	}
-	else if (boost::filesystem::exists(parent_path/au_loc_path))
+	else if (boost::filesystem::exists(parent_path / au_loc_path))
 	{
-		au_loc = (parent_path/au_loc_path).string();
+		au_loc = (parent_path / au_loc_path).string();
 	}
-	else if (boost::filesystem::exists(config_path/au_loc_path))
+	else if (boost::filesystem::exists(config_path / au_loc_path))
 	{
-		au_loc = (config_path/au_loc_path).string();
+		au_loc = (config_path / au_loc_path).string();
 	}
 	else
 	{
@@ -283,32 +283,35 @@ int main (int argc, char **argv)
 
 	//Create face Action Units (AU) analyser 
 	FaceAnalysis::FaceAnalyser face_analyser(vector<cv::Vec3d>(), sim_scale, sim_size, sim_size, au_loc, tri_loc);
-	
+
 	//End of Action Units Extraction
 
 
-	while(!done) // this is not a for loop as we might also be reading from a webcam
+	while (!done) // this is not a for loop as we might also be reading from a webcam
 	{
-		
+
 		string current_file;
 
 		// We might specify multiple video files as arguments
-		if(files.size() > 0)
+		if (files.size() > 0)
 		{
-			f_n++;			
-		    current_file = files[f_n];
+			f_n++;
+			current_file = files[f_n];
 		}
 		else
 		{
 			// If we want to write out from webcam
 			f_n = 0;
 		}
-		
-		bool use_depth = !depth_directories.empty();	
+
+		bool use_depth = !depth_directories.empty();
 
 		// Do some grabbing
 		cv::VideoCapture video_capture;
-		if( current_file.size() > 0 )
+		video_capture.set(cv::CAP_PROP_FPS, 60);
+		video_capture.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+		video_capture.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+		if (current_file.size() > 0)
 		{
 			if (!boost::filesystem::exists(current_file))
 			{
@@ -318,13 +321,13 @@ int main (int argc, char **argv)
 
 			current_file = boost::filesystem::path(current_file).generic_string();
 
-			INFO_STREAM( "Attempting to read from file: " << current_file );
-			video_capture = cv::VideoCapture( current_file );
+			INFO_STREAM("Attempting to read from file: " << current_file);
+			video_capture = cv::VideoCapture(current_file);
 		}
 		else
 		{
-			INFO_STREAM( "Attempting to capture from device: " << device );
-			video_capture = cv::VideoCapture( device );
+			INFO_STREAM("Attempting to capture from device: " << device);
+			video_capture = cv::VideoCapture(device);
 
 			// Read a first frame often empty in camera
 			cv::Mat captured_image;
@@ -336,10 +339,10 @@ int main (int argc, char **argv)
 			FATAL_STREAM("Failed to open video source");
 			return 1;
 		}
-		else INFO_STREAM( "Device or file opened");
+		else INFO_STREAM("Device or file opened");
 
 		cv::Mat captured_image;
-		video_capture >> captured_image;		
+		video_capture >> captured_image;
 
 		// If optical centers are not defined just use center of image
 		if (cx_undefined)
@@ -355,46 +358,46 @@ int main (int argc, char **argv)
 
 			fx = (fx + fy) / 2.0;
 			fy = fx;
-		}		
-	
+		}
+
 		int frame_count = 0;
-		
+
 		// saving the videos
 		cv::VideoWriter writerFace;
 		if (!output_video_files.empty())
 		{
 			try
- 			{
+			{
 				writerFace = cv::VideoWriter(output_video_files[f_n], CV_FOURCC(output_codec[0], output_codec[1], output_codec[2], output_codec[3]), 30, captured_image.size(), true);
 			}
-			catch(cv::Exception e)
+			catch (cv::Exception e)
 			{
-				WARN_STREAM( "Could not open VideoWriter, OUTPUT FILE WILL NOT BE WRITTEN. Currently using codec " << output_codec << ", try using an other one (-oc option)");
+				WARN_STREAM("Could not open VideoWriter, OUTPUT FILE WILL NOT BE WRITTEN. Currently using codec " << output_codec << ", try using an other one (-oc option)");
 			}
 		}
 
 		// Use for timestamping if using a webcam
 		int64 t_initial = cv::getTickCount();
 
-		INFO_STREAM( "Starting tracking");
-		while(!captured_image.empty())
-		{		
+		INFO_STREAM("Starting tracking");
+		while (!captured_image.empty())
+		{
 
 			// Reading the images
 			cv::Mat_<float> depth_image;
 			cv::Mat_<uchar> grayscale_image;
 
-			if(captured_image.channels() == 3)
+			if (captured_image.channels() == 3)
 			{
-				cv::cvtColor(captured_image, grayscale_image, CV_BGR2GRAY);				
+				cv::cvtColor(captured_image, grayscale_image, CV_BGR2GRAY);
 			}
 			else
 			{
-				grayscale_image = captured_image.clone();				
+				grayscale_image = captured_image.clone();
 			}
-		
+
 			// Get depth image
-			if(use_depth)
+			if (use_depth)
 			{
 				char* dst = new char[100];
 				std::stringstream sstream;
@@ -405,19 +408,19 @@ int main (int argc, char **argv)
 				cv::Mat_<short> depth_image_16_bit = cv::imread(string(dst), -1);
 
 				// Convert to a floating point depth image
-				if(!depth_image_16_bit.empty())
+				if (!depth_image_16_bit.empty())
 				{
 					depth_image_16_bit.convertTo(depth_image, CV_32F);
 				}
 				else
 				{
-					WARN_STREAM( "Can't find depth image" );
+					WARN_STREAM("Can't find depth image");
 				}
 			}
-			
+
 			// The actual facial landmark detection / tracking
 			bool detection_success = LandmarkDetector::DetectLandmarksInVideo(grayscale_image, depth_image, clnf_model, det_parameters);
-			
+
 			// Visualising the results
 			// Drawing the facial landmarks on the face and the bounding box around it if tracking is successful and initialised
 			double detection_certainty = clnf_model.detection_certainty;
@@ -434,7 +437,7 @@ int main (int argc, char **argv)
 
 			//Visualize Tracking Data
 			visualise_tracking(captured_image, depth_image, clnf_model, det_parameters, gazeDirection0, gazeDirection1, frame_count, fx, fy, cx, cy);
-			
+
 			//Send Tracking data over OSC
 			OSC_Funcs::OSC_Transmitter::SendFaceData(clnf_model, gazeDirection0, gazeDirection1, fx, fy, cx, cy, -1);
 
@@ -450,17 +453,17 @@ int main (int argc, char **argv)
 
 
 			video_capture >> captured_image;
-		
+
 			// detect key presses
 			char character_press = cv::waitKey(1);
-			
+
 			// restart the tracker
-			if(character_press == 'r')
+			if (character_press == 'r')
 			{
 				clnf_model.Reset();
 			}
 			// quit the application
-			else if(character_press=='q')
+			else if (character_press == 'q')
 			{
 				return(0);
 			}
@@ -469,14 +472,14 @@ int main (int argc, char **argv)
 			frame_count++;
 
 		}
-		
+
 		frame_count = 0;
 
 		// Reset the model, for the next video
 		clnf_model.Reset();
-		
+
 		// break out of the loop if done with all the files (or using a webcam)
-		if(f_n == files.size() -1 || files.empty())
+		if (f_n == files.size() - 1 || files.empty())
 		{
 			done = true;
 		}
